f9facae64972d7836d5da5d9240d57421a7bfa0d
lateef0407 <63701286+lateef0407@users.noreply.github.com>
2020-07-19 20:58:27 +0530

    Create calculator.js

---
+++ DAY 4 Assignment/calculator.js
@@ -0,0 +1,20 @@
+console.log("Basic Calculator")
+let a = 10
+let b = 5
+console.log("Addition")
+let c = a + b ;
+console.log(c);
+console.log("Subtraction")
+let d = a - b ;
+console.log(d);
+console.log("Multiplication")
+let e = a* b ;
+console.log(e);
+console.log("Divison")
+let f = a / b ;
+console.log(f);
+console.log("Square Root")
+console.log(Math.sqrt(b));
+console.log("Percentage")
+let g = a%b;
+console.log(g);
