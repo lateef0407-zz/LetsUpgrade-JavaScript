3318dc07e63ff6fa2dc31c31f006024b25261f2a
lateef0407 <63701286+lateef0407@users.noreply.github.com>
2020-07-19 21:02:34 +0530

    Create shopping.js

---
+++ DAY 4 Assignment/shopping.js
@@ -0,0 +1,7 @@
+console.log("I am going for shopping");
+shoppingList = ["Milk","Sugar","Wheat","Chocolates"];
+console.log(shoppingList);
+let shoppingBasket = [...shoppingList];
+console.log(shoppingBasket);
+shoppingList.push("Candies");
+console.log(shoppingList);
